017e89d0843becc25c160658e71020ca0a9e290a
agaesh <agaeshk@gmail.com>
2025-05-21 22:38:20 +0800

    Add CoreEmployeeModel with fields and table creation

--- model/CoreEmployeeModel.py
+++ model/CoreEmployeeModel.py
@@ -3,3 +3,14 @@ from sqlalchemy import Date,String,Column, Integer,Float
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
+class CoreEmployeeModel(Base):
+    _tablename_ = "Employee"
+    id = Column(Integer, primary_key=True, autoincrement= True)
+    firstname = Column(String(50), nullable=False)
+    lastname = Column(String(50), nullable =False)
+    age = Column(Integer, nullable= False)
+    email = Column(String(50), nullable = True, unique= True)
+    hire_date = Column(Date,nullable=False, default=Date.date.today)
+
+Base.metadata.create_all(get_connection)
