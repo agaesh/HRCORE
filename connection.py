98af9a10b6fbd60c94423049d92355a3ea6ece96
agaesh <agaeshk@gmail.com>
2025-05-22 12:24:15 +0800

    Refactor database connection to use SQLAlchemy engine instead of pyodbc

--- connection.py
+++ connection.py
@@ -1,15 +1,9 @@
-import pyodbc
+from sqlalchemy import create_engine
 
 def get_connection():
-    try:
-        conn = pyodbc.connect(
-            r"DRIVER={SQL SERVER};"
-            r"SERVER=LAPTOP-0QQ7OVBA\SQLEXPRESS01;"
-            r"DATABASE=HRCORE;"
-            r"UID=sa;"
-            r"PWD=mcl@1234"
-        )
-        return conn
-    except Exception as e:
-        print(e)
-
+    connection_string = (
+       "mssql+pyodbc://sa:mcl%401234@LAPTOP-0QQ7OVBA\\SQLEXPRESS01/HRCORE"
+       "?driver=SQL+Server"
+    )
+    engine = create_engine(connection_string)
+    return engine
