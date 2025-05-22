076f250f44af7ae952fbbf5486aaf354c2d65d55
agaesh <agaeshk@gmail.com>
2025-05-22 12:23:51 +0800

    commiting all

--- model/CoreEmployeeModel.py
+++ model/CoreEmployeeModel.py
@@ -1,8 +1,9 @@
-from connection import get_connection
+
 from sqlalchemy import Date,String,Column, Integer,Float
 from sqlalchemy.ext.declarative import declarative_base
 import datetime
 
+engine = get_connection
 Base = declarative_base()
 
 class CoreEmployeeModel(Base):
@@ -14,4 +15,10 @@ class CoreEmployeeModel(Base):
     email = Column(String(50), nullable = True, unique= True)
     hire_date = Column(Date,nullable=False, default=datetime.date.today)
 
-Base.metadata.create_all(get_connection)
+
+# Check if the table exists, then create it if not
+if not engine.dialect.has_table(engine, CoreEmployeeModel.__tablename__):
+    Base.metadata.create_all(engine)
+    print(f"Table '{CoreEmployeeModel.__tablename__}' has been created.")
+else:
+    print(f"Table '{CoreEmployeeModel.__tablename__}' already exists.")
